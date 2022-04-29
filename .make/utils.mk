16a00b9bc9601838fde881f353f14d60ec553d57
Nil Borodulia <nborodulia@gmail.com>
2022-04-29 22:39:14 +0100

    fix perm

--- .make/utils.mk
+++ .make/utils.mk
@@ -14,7 +14,7 @@ fix-permission: ## fix permission for docker env
 	echo chown -R $(shell whoami):$(shell whoami) *
 	echo chown -R $(shell whoami):$(shell whoami) .docker/*
 	echo chmod +x ./bin/console
-	echo chmod 777 ./tests/reports
+	echo chmod -R 0777 ./tests/reports
 
 wait:
 ifeq ($(OS),Windows_NT)

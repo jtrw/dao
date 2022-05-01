d99c531d21682f9028134472c2bc2aa7dbead96f
Nil Borodulia <nborodulia@gmail.com>
2022-05-01 21:41:36 +0100

    debig

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,29 @@
 <?php
 require __DIR__."/../vendor/autoload.php";
 
+echo get_current_user()."\n";
+
+try {
+    $result = file_put_contents(__DIR__."/reports/test.xml", "Hello");
+    if (!$result) {
+        echo "LOL!";
+    }
+} catch (Throwable $th) {
+    echo $th->getMessage();
+}
+
+
+$cmd = "chmod -R 0777 ".__DIR__."/reports";
+`$cmd`;
+
+try {
+    $result = file_put_contents(__DIR__."/reports/test.xml", "Hello");
+    if (!$result) {
+        echo "2)LOL!";
+    }
+} catch (Throwable $th) {
+    echo "2)".$th->getMessage();
+}
+
 \Jtrw\DAO\Tests\DbConnector::init();
 

ecd04975e521c5883d0d9c4ce7811048b72b5b7d
Nil Borodulia <nborodulia@gmail.com>
2022-05-04 23:29:38 +0100

    add test objectAdapter

--- src/DataAccessObject.php
+++ src/DataAccessObject.php
@@ -5,8 +5,6 @@ use Jtrw\DAO\Exceptions\DatabaseException;
 
 abstract class DataAccessObject implements DataAccessObjectFactoryInterface
 {
-    private static $_instances;
-
     public static function factory(object $connection): DataAccessObjectInterface
     {
         if ($connection instanceof DataAccessObjectInterface) {
@@ -14,10 +12,6 @@ abstract class DataAccessObject implements DataAccessObjectFactoryInterface
         }
 
         $className = static::getAdapterClassName($connection);
-
-        if (!$className) {
-            throw new DatabaseException('Not found an adapter class for storage connection.');
-        }
 
         return static::create($className, $connection);
     } // end factory
@@ -40,36 +34,10 @@ abstract class DataAccessObject implements DataAccessObjectFactoryInterface
             case 'PDO':
                 $adapterName = 'PDO';
                 break;
-
             default:
                 throw new DatabaseException("Adapter Not Found");
         }
 
         return 'Object' . $adapterName . 'Adapter';
     } // end getAdapterClassName
-
-    public static function &getInstance(string $name, object $connection): DataAccessObjectInterface
-    {
-        if (isset(self::$_instances[$name])) {
-            return self::$_instances[$name];
-        }
-
-        self::$_instances[$name] = self::createInstance($name, $connection);
-
-        return self::$_instances[$name];
-    } // end getInstance
-
-    public static function createInstance(string $name, object $connection): DataAccessObjectInterface
-    {
-        $adapter = self::factory($connection);
-
-        $className = self::getClassName($name);
-
-        if (!class_exists($className)) {
-            throw new DatabaseException('Not found object class: '.$className);
-        }
-        $className = 'Jtrw\DAO\\'.$className;
-
-        return new $className($adapter);
-    } // end createInstance
 }
